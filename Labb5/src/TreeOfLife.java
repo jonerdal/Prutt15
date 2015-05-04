c20355ce3bf552c0c43553d8cd669366e8aa27ea
Jonatan Erdal <jerdal@csc.kth.se>
2015-05-04 16:46:06 +0200

    labb5 med fler kommentarer

--- Labb5/src/TreeOfLife.java
+++ Labb5/src/TreeOfLife.java
@@ -23,6 +23,11 @@ public class TreeOfLife extends TreeFrame {
 		tree = new JTree(treeModel);
 		buildTree();
 	}
+	
+	public static void main(String[] args) {
+		
+		new TreeOfLife();
+	}
 
 
 	private void buildTree() {
@@ -38,7 +43,7 @@ public class TreeOfLife extends TreeFrame {
 		DefaultMutableTreeNode child = new DefaultMutableTreeNode(x.getName());
 		parent.add(child);
 		
-		//går igenom listan med barnets barn
+		//går igenom listan med barnets barn och bygger träd för dessa
 		for(MyNode y : x.getChildrenList()) {
 			buildTree(y,child);
 		}
@@ -54,18 +59,17 @@ public class TreeOfLife extends TreeFrame {
 		//hittar rätt node och hämtar description för denna
 		for (MyNode x : allNodes) {
 			if (x.getName().equals(path.getLastPathComponent().toString())) {
+				
 				JOptionPane.showMessageDialog( this, x.getDescription());
 			}
 		}
 	}
 
-	public static void main(String[] args) {
-		
-		new TreeOfLife();
-	}
+
 
 	private void readFile(String file) {
-		// läser in fil och populerar de tre listorna arr0-arr2
+		
+		// Kontrollerar så det inte blir exception när vi läser in filen
 		try {
 			sc = new Scanner(new File(file));
 
@@ -83,20 +87,29 @@ public class TreeOfLife extends TreeFrame {
 				//avslutande tagg, kontrollera att den hör till rätt starttagg
 				String tempArr[] = line.split("/|>",3);
 				String temp = tempArr[1];
+				//kollar om det är samma
 				if(temp.equals(myNodes.get(myNodes.size()-1).getLevel())) { 
+					
+					//kontrollerar så att vi inte försöker ta bort sista objektet, roten
 					if (myNodes.size()>1) {
+						//berättar för nodens farsa om dess barn
 						myNodes.get(myNodes.size()-2).addChild(myNodes.get(myNodes.size()-1));
+						//tar bort barnet, det har ju inga barn eftersom vi hittat en sluttagg
 						myNodes.remove(myNodes.size()-1);
 					}
 				}
+				
 				else {
-					System.out.println("Felaktig tagg");
+					//felaktig tagg, stänger av programmet
+					System.out.println("Felaktig tagg, avslutar process");
 					System.exit(getDefaultCloseOperation());
 				}
+			
 			}
 			else if (String.valueOf(line.charAt(1)).equals("?")) {
-				//inforad om xml, hoppa över denna!
+				//inforad om xml, hoppar över denna!
 			}
+			
 			else {
 				String tempArr[] = line.split("<| |\"",6);
 				//skapar ny node
